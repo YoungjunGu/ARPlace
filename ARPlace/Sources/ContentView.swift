86ae865435510f80eb0827d41ead87ad075f1b55
gaki <guyeongjun@naver.com>
2020-04-13 14:10:56 +0900

    프로젝트 세팅

---
+++ ARPlace/Sources/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ARPlace
+//
+//  Created by youngjun goo on 2020/04/13.
+//  Copyright © 2020 youngjun goo. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
