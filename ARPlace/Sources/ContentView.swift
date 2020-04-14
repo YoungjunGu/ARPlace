df5c2ebe5af455d4c8a1e9ba087e8b46e56d61ec
gaki <guyeongjun@naver.com>
2020-04-15 00:57:18 +0900

    gitignore 수정 & NaverMaps 세팅

--- ARPlace/Sources/ContentView.swift
+++ ARPlace/Sources/ContentView.swift
@@ -7,15 +7,42 @@
 //
 
 import SwiftUI
+import NMapsMap
 
-struct ContentView: View {
-    var body: some View {
-        Text("Hello, World!")
+struct NaverMapView: UIViewRepresentable {
+    
+    var locationManager = CLLocationManager()
+    
+    func setUpManager() {
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.requestAlwaysAuthorization()
+    }
+    
+    func makeUIView(context: Context) -> NMFNaverMapView {
+        setUpManager()
+        let naverMapView = NMFNaverMapView()
+        naverMapView.positionMode = .disabled
+        naverMapView.mapView.zoomLevel = 15
+        naverMapView.showLocationButton = false
+        naverMapView.showIndoorLevelPicker = true
+        return naverMapView
+    }
+    
+    func updateUIView(_ uiView: NMFNaverMapView, context: Context) {
     }
 }
 
+struct ContentView: View {
+    var body: some View {
+        NaverMapView()
+    }
+}
+
+#if DEBUG
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
 }
+#endif
---
+++ ARPlace/Sources/MainView.swift
@@ -0,0 +1,21 @@
+//
+//  MainView.swift
+//  ARPlace
+//
+//  Created by youngjun goo on 2020/04/14.
+//  Copyright © 2020 youngjun goo. All rights reserved.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MainView_Previews: PreviewProvider {
+    static var previews: some View {
+        MainView()
+    }
+}
